00cbf77647887645c540750235fa50d93833ea6b
Yarema87 <119173438+Yarema87@users.noreply.github.com>
2023-03-30 15:51:25 +0300

    Update Chainsaw.java

--- Chainsaw.java
+++ Chainsaw.java
@@ -7,41 +7,32 @@ import lombok.*;
 @Getter
 @ToString
 
-public class Chainsaw extends Saws{
-    private String brand;
-    private int power;
+public class Chainsaw extends Saw {
     private double fuelTankCapacity;
     private double fuelLevel;
-    private boolean isWorking;
-    private final boolean isAutomatic = true;
-    private final int price = 4000;
-    private static Chainsaw instance;
     public boolean start(){
         setWorking(true);
-        return isWorking;
+        return getWorking();
     }
     public boolean stop(){
         setWorking(false);
-        return isWorking;
-    }
-    @Override
-    public boolean cutWood(double length) {
-        if (fuelLevel >= length * 0.1) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return getWorking();
     }
     public double charge(double fuel){
         this.fuelLevel = fuelLevel + fuel;
         return this.fuelLevel;
     }
-    public String convertingToString(){
+    public String convertToString(){
         Chainsaw chainsaw = new Chainsaw();
         return chainsaw + "";
     }
-    public static Chainsaw getInstance(){
-        return new Chainsaw();
+    @Override
+    public double getRemainingWorkTime() {
+        return (fuelLevel / 0.6);
+    }
+    public Chainsaw(String brand, int power, boolean isWorking, double workingTimeInHours, double fuelTankCapacity, double fuelLevel){
+        super(brand, power, isWorking, workingTimeInHours);
+        this.fuelTankCapacity = fuelTankCapacity;
+        this.fuelLevel = fuelLevel;
     }
 }
