ef49dfa1b19b6c3f38dbcaacd02a53779314f626
Yarema87 <119173438+Yarema87@users.noreply.github.com>
2023-04-06 18:44:15 +0300

    Update CircularSaw.java

--- CircularSaw.java
+++ CircularSaw.java
@@ -1,5 +1,10 @@
-package ua.lviv.iot.algo.part1.lab1;
-import lombok.*;
+package main.java.ua.lviv.iot.algo.part1.lab1;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import main.java.ua.lviv.iot.algo.part1.lab1.Saw;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -7,26 +12,29 @@ import lombok.*;
 @Getter
 @ToString
 
-public class CircularSaw extends Saw{
+public class CircularSaw extends Saw {
     private double radius;
     private double width;
-    public boolean start(){
+    public boolean start() {
         setWorking(true);
         return getWorking();
     }
-    public boolean stop(){
+    public boolean stop() {
         setWorking(false);
         return getWorking();
     }
-    public String convertToString(){
+    public String convertToString() {
         CircularSaw circularsaw = new CircularSaw();
         return circularsaw + "";
     }
     @Override
     public double getRemainingWorkTime() {
-        return 8;
+        int timeWithElectricityOn = 8;
+        return timeWithElectricityOn;
     }
-    public CircularSaw(String brand, int power, boolean isWorking, double workingTimeInHours, double radius, double width) {
+    public CircularSaw(final String brand, final int power,
+                       final boolean isWorking, final double workingTimeInHours,
+                       final double radius, final double width) {
         super(brand, power, isWorking, workingTimeInHours);
         this.radius = radius;
         this.width = width;
