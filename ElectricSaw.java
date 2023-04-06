11ab029b3a3daa204fa72a25587aa4e7dbc8122f
Yarema87 <119173438+Yarema87@users.noreply.github.com>
2023-04-06 18:44:51 +0300

    Update ElectricSaw.java

--- ElectricSaw.java
+++ ElectricSaw.java
@@ -1,5 +1,10 @@
-package ua.lviv.iot.algo.part1.lab1;
-import lombok.*;
+package main.java.ua.lviv.iot.algo.part1.lab1;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import main.java.ua.lviv.iot.algo.part1.lab1.Saw;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -9,27 +14,31 @@ import lombok.*;
 
 public class ElectricSaw extends Saw {
     private int batteryCharge;
-    public double charge(int charge){
+    public double charge(final int charge) {
         this.batteryCharge = batteryCharge + charge;
         return this.batteryCharge;
     }
-    public boolean start(){
+    public boolean start() {
         setWorking(true);
         return getWorking();
     }
-    public boolean stop(){
+    public boolean stop() {
         setWorking(false);
         return getWorking();
     }
-    public String convertToString(){
+    public String convertToString() {
         ElectricSaw electricSaw = new ElectricSaw();
         return electricSaw + "";
     }
     @Override
     public double getRemainingWorkTime() {
-        return (batteryCharge * 1.5 / 60);
+        double dischargeBatteryPerMinute = 1.5;
+        int minutesInHour = 60;
+        return batteryCharge * dischargeBatteryPerMinute / minutesInHour;
     }
-    public ElectricSaw(String brand, int power, boolean isWorking, double workingTimeInHours, int batteryCharge){
+    public ElectricSaw(final String brand, final int power,
+                       final boolean isWorking, final double workingTimeInHours,
+                       final int batteryCharge) {
         super(brand, power, isWorking, workingTimeInHours);
         this.batteryCharge = batteryCharge;
     }
